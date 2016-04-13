8279040641a1a9ebf94ad633b527c54089706fd4
Parminder Singh <preetzdomain@gmail.com>
2016-04-13 18:21:35 +1000

    added login dummy template

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,16 +13,32 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/node_modules')));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// SESSION SETUP
+app.use(session(
+    {
+      secret: 'projectManagementAgileWay',
+      saveUninitialized: true,
+      resave: true
+    }
+));
+
 
 app.use('/', routes);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('index', {
+    title: 'Login | VLBC',
+    email: '',
+    info: ''
+  });
+});
+
+
+router.post('/', function(req, res, next){
+
+  var email = req.body.email;
+  var pass = req.body.password;
+
+  console.log(email);
+  console.log(pass);
+
+
+  res.render('index', {
+    title: 'Home | VLBC',
+    email: email,
+    password: pass
+  })
 });
 
 module.exports = router;

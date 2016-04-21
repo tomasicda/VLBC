4f10cc438fb2d69c7692920550fca177be96d8b0
DannyTomasic <danieltomasic@Dannys-MacBook-Pro.local>
2016-04-21 20:05:30 +1000

    Login system is wokring now
    Seesions are in place as well

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var path = require('path');
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
+var dbConnections = require('./DAO/DBConnection');
 var session = require('express-session');
+var mongoose = require ('mongoose');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,26 +21,22 @@ app.use(express.static(path.join(__dirname, '/node_modules')));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// SESSION SETUP
+//SESSION SETUP
 app.use(session(
-    {
+    {   
       secret: 'projectManagementAgileWay',
       saveUninitialized: true,
       resave: true
     }
 ));
 
-
 app.use('/', routes);
 app.use('/users', users);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -70,6 +68,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;

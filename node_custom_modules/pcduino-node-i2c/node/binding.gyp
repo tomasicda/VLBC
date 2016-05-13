ea91a64dabffc5062f155b0a157fdc1e537bd834
Parminder Singh <preetzdomain@gmail.com>
2016-05-13 23:50:11 +1000

    i2c node module boiler-plate code

---
+++ node_custom_modules/pcduino-node-i2c/node/binding.gyp
@@ -0,0 +1,8 @@
+{
+  "targets": [
+    {
+      "target_name": "pcduino-i2c",
+      "sources": [  "pcduino-i2c.cc"]
+    }
+  ]
+}
---
+++ node_custom_modules/pcduino-node-i2c/node/build/Release/.deps/Release/obj.target/pcduino-i2c.node.d
@@ -0,0 +1,1 @@
+cmd_Release/obj.target/pcduino-i2c.node := g++ -shared -pthread -rdynamic  -Wl,-soname=pcduino-i2c.node -o Release/obj.target/pcduino-i2c.node -Wl,--start-group Release/obj.target/pcduino-i2c/pcduino-i2c.o -Wl,--end-group 

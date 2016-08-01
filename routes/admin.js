a0bc96084732e84d8252897682328666d67018f4
Parminder Singh <preetzdomain@gmail.com>
2016-08-01 18:31:18 +1000

    Comment out ic2 require module in manualSwitch.js to enable project to run on development machine. Added side menu bar on admin page. Added bootstrap modal front end to add load profile. Has set up ajax call function to add load profile as well.

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var admin = express.Router();
 var User = require('../Models/User');
+var relayChannel = require('../Models/relayChannel');
 var dbConnection = require('../DAO/DBConnection');
 var restrict = require('../DAO/Session');
 
@@ -10,5 +11,25 @@ admin.get('/', restrict,function (req, res, next) {
         title: 'admin | VLBC'
     });
 });
+
+/* GET home page. */
+admin.post('/loadProfiles', restrict,function (req, res, next) {
+
+    var title = req.body.title;
+
+    // relayChannel.find(function (err, channels) {
+    //     if (err){
+    //         console.log(err);
+    //         return res.status(500).send();
+    //     }
+    //     if (!channels){
+    //         return res.status(401).send();
+    //     }
+    //
+    //     res.send(channels);
+    // });
+
+});
+
 
 module.exports = admin;

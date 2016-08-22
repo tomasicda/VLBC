fd24aacb9e202a287972576803f6550ec8cb7f56
Parminder Singh <preetzdomain@gmail.com>
2016-08-23 03:06:36 +1000

    Simulating 'load time data set' timer has been configured corrently now.

--- Helpers/LoadProfileManager.js
+++ Helpers/LoadProfileManager.js
@@ -7,181 +7,117 @@ var LoadProfileManager = module.exports = {
 
     timeSheduler: function () {
 
-        var date1 = new Date();
-        var currentHours1 = date1.getHours();
-        var currentMinutes1 = date1.getMinutes();
-        var currentSeconds1 = date1.getSeconds();
+        var date = new Date();
+        var currentHours = date.getHours();
+        var currentMinutes = date.getMinutes();
+        var currentSeconds = date.getSeconds();
+        var calculateTimeLeftFor = 'today';
+        var query = {Time:{$gt:{Hours: currentHours, Minutes: currentMinutes}}};
 
-        console.log("\n||===============================================");
-        console.log("SYSTEM STARTS AT TIME : " + currentHours1+ ":" + currentMinutes1 + ":" + currentSeconds1 + " HMS");
+        console.log("\n||==============================================");
+        console.log("SYSTEM STARTED AT -> " + currentHours+ ":" + currentMinutes + ":" + currentSeconds + " HMS");
+        console.log("||==============================================");
 
-        // find and run current load profile when system starts
-        loadProfile.findOne({ Time: {$lte: {Hours: currentHours1, Minutes: currentMinutes1} } }).sort({Time: -1}).limit(1).exec(function (err, startProfile) {
+        // find and run current simulation (Load And Time Data Set) from load profile when system starts
+        loadProfile.findOne({ Time: {$lte: {Hours: currentHours, Minutes: currentMinutes} } }).sort({Time: -1}).limit(1).exec(function (err, loadTimeDataSet) {
 
             if(err) throw err;
 
-            if(startProfile === null) {
-                console.log("\n||===============================================");
-                console.log("NO CURRENT PROFILE TO RUN");
+            if(loadTimeDataSet === null) {
+                console.log("\n||==============================================");
+                console.log("SYSTEM CAN'T FIND LOAD TIME DATA SET TO SIMULATE");
             } else {
-                console.log("\n||===============================================");
-                console.log("RUNNING CURRENT PROFILE.......Power: " + startProfile.Power + ".......TIME " + startProfile.Time.Hours + ":" + startProfile.Time.Minutes);
-                //i2c.automaticUpdate(startProfile);
+                console.log("\n||==============================================");
+                console.log("RUNNING CURRENT SIMULATION DATA SET......Power: " + loadTimeDataSet.Power + ".......TIME " + loadTimeDataSet.Time.Hours + ":" + loadTimeDataSet.Time.Minutes);
+                //i2c.automaticUpdate(loadTimeDataSet);
             }
         });
 
+        // find next simulation load time data set and calculate seconds left to start
+        function getNextSimulationDataSet(query, calculateTimeLeftFor, callback) {
 
-        // find and get next profile details
-        function findNextLoadProfile(callback) {
+            var nextLoadTimeDataSetWithTimeLeftToRun = null;
 
-            var nextLoadProfileWithTimeOut = null;
-
-            var date2 = new Date();
-            var currentHours2 = date2.getHours();
-            var currentMinutes2 = date2.getMinutes();
-
-
-            loadProfile.findOne({ Time: {$gt: { Hours: currentHours2, Minutes: currentMinutes2 }} }).exec(function (err, profile) {
+            loadProfile.findOne(query).exec(function (err, loadTimeDataSet) {
 
                 if (err) throw err;
 
-                // getting current time to compare with next load profile time
-                var date3 = new Date();
-                var currentHours3 = date3.getHours();
-                var currentMinutes3 = date3.getMinutes();
-                var currentSeconds3 = date3.getSeconds();
-
-                // IF COULD NOT FIND NEXT LOAD PROFILE IN DATABASE THEN GET FIRST ONE IF EXISTS.
-                if (profile === null) {
-                    console.log("\n||===============================================");
-                    console.log("COULD NOT FIND NEXT LOAD PROFILE IN DATABASE");
-
-                    var nextProfile;
-
-                    // get first profile with lowest number in Time
-                    loadProfile.findOne({}, {}, {sort: {Time: 1}}, function(err, profile) {
-
-                        if(err) throw err;
-
-                        if(profile === null) {
-
-                            console.log("\n||===============================================");
-                            console.log("NO LOAD PROFILE FOUND.");
-                            // do nothing....
-
-                        } else {
-
-                            console.log("\n||===============================================");
-                            console.log("NEXT LOAD PROFILE TO RUN........POWER: " + profile.Power + "........TIME " + profile.Time.Hours + ":" + profile.Time.Minutes);
-
-                            // getting current time to compare with next load profile time
-                            var date3 = new Date();
-                            var currentHours3 = date3.getHours();
-                            var currentMinutes3 = date3.getMinutes();
-                            var currentSeconds3 = date3.getSeconds();
-
-                            // calculate and return time left for setInterval or timeout
-                            var nextLoadProfileTimeInSeconds = hoursToSeconds(profile.Time.Hours) + minutesToSeconds(profile.Time.Minutes);
-                            var currentTimeInSeconds = hoursToSeconds(currentHours3) + minutesToSeconds(currentMinutes3) + currentSeconds3;
-                            var timeToNextDay = 86400 - currentTimeInSeconds;
-                            var secondsLeftForTimeOut =  timeToNextDay + nextLoadProfileTimeInSeconds;
-
-                            profile.TimeOut = secondsLeftForTimeOut * 1000 ;
-
-                            function hoursToSeconds(hours){
-                                return hours * 60 * 60;
-                            }
-
-                            function minutesToSeconds(minutes){
-                                return minutes * 60;
-                            }
-
-                        }
-
-                        nextProfile = profile;
-
-
-                        //nextLoadProfileWithTimeOut = nextProfile;
-
-                        console.log("nextProfileNEXTNEXTPROFILE: " + nextProfile.TimeOut);
-
-
-                    });
-
-                    //nextLoadProfileWithTimeOut = nextProfile;
-
+                if (loadTimeDataSet === null) {
+                    // callback returns null and handled by execution callback function
 
                 } else {
 
-                    console.log("\n||===============================================");
-                    console.log("NEXT LOAD PROFILE TO RUN........POWER: " + profile.Power + "........TIME " + profile.Time.Hours + ":" + profile.Time.Minutes);
+                    var date = new Date();
+                    var nextLoadProfileTimeInSeconds, currentTimeInSeconds, secondsLeftForTimeOut;
 
-                    // calculate and return time left for setInterval or timeout
-                    var nextLoadProfileTimeInSeconds = hoursToSeconds(profile.Time.Hours) + minutesToSeconds(profile.Time.Minutes);
-                    var currentTimeInSeconds = hoursToSeconds(currentHours3) + minutesToSeconds(currentMinutes3) + currentSeconds3;
-                    var secondsLeftForTimeOut = nextLoadProfileTimeInSeconds - currentTimeInSeconds;
-
-                    console.log("\n||===============================================");
-                    console.log("nextLoadProfileTimeInSeconds " + nextLoadProfileTimeInSeconds);
-                    console.log("currentTimeInSeconds " + currentTimeInSeconds);
-                    console.log("secondsLeftForTimeOut " + secondsLeftForTimeOut);
-
-
-                    profile.TimeOut = secondsLeftForTimeOut * 1000 ;
-
-                    function hoursToSeconds(hours){
+                    function convertHoursToSeconds(hours){
                         return hours * 60 * 60;
                     }
 
-                    function minutesToSeconds(minutes){
+                    function convertMinutesToSeconds(minutes){
                         return minutes * 60;
                     }
 
-                    nextLoadProfileWithTimeOut = profile;
+                    if(calculateTimeLeftFor === 'today') {
+                        nextLoadProfileTimeInSeconds = convertHoursToSeconds(loadTimeDataSet.Time.Hours) + convertMinutesToSeconds(loadTimeDataSet.Time.Minutes);
+                        currentTimeInSeconds = convertHoursToSeconds(date.getHours()) + convertMinutesToSeconds(date.getMinutes()) + date.getSeconds();
+                        secondsLeftForTimeOut = nextLoadProfileTimeInSeconds - currentTimeInSeconds;
 
+                    } else if (calculateTimeLeftFor === 'nextDay') {
+                        nextLoadProfileTimeInSeconds = convertHoursToSeconds(loadTimeDataSet.Time.Hours) + convertMinutesToSeconds(loadTimeDataSet.Time.Minutes);
+                        currentTimeInSeconds = convertHoursToSeconds(date.getHours()) + convertMinutesToSeconds(date.getMinutes()) + date.getSeconds();
+                        var timeToNextDay = 86400 - currentTimeInSeconds;
+                        secondsLeftForTimeOut =  timeToNextDay + nextLoadProfileTimeInSeconds;
+                    }
+
+                    loadTimeDataSet.TimeLeft = secondsLeftForTimeOut * 1000 ;
+                    nextLoadTimeDataSetWithTimeLeftToRun = loadTimeDataSet;
                 }
 
-                callback(nextLoadProfileWithTimeOut);
-            });
+                callback(nextLoadTimeDataSetWithTimeLeftToRun);
 
-        }
+            }); //database query ends here
 
+        } //getNextSimulationDataSet ends here
 
-        runNextLoadProfile();
+        setupNextSimulation(query, calculateTimeLeftFor);
 
+        function setupNextSimulation(query, calculateTimeLeftFor) {
 
-        function runNextLoadProfile() {
+            getNextSimulationDataSet(query, calculateTimeLeftFor, function(loadTimeDataSet) {
 
-            findNextLoadProfile(function(profile) {
-
-                if (profile === null) {
-                    // do nothing...
-                    console.log("JUST BEFORE TIME OUT SETUP, PROFILE FOUND NULLLLLLL");
+                if (loadTimeDataSet === null) {
+                    console.log("::::::::::NEXT SIMULATION STARTS TOMORROW:::::::::");
+                    var calculateTimeLeftFor = 'nextDay';
+                    var query  = {Time:{$gte:{Hours: 0, Minutes: 0}}};
+                    setupNextSimulation(query, calculateTimeLeftFor);
 
                 } else {
 
+                    console.log("NEXT SIMULATION DATA SET TO RUN......POWER: " + loadTimeDataSet.Power + "........TIME " + loadTimeDataSet.Time.Hours + ":" + loadTimeDataSet.Time.Minutes);
+                    console.log("||==============================================");
+
                     setTimeout(function () {
 
-                        console.log("\n||===============================================");
-                        console.log("RUNNING CURRENT PROFILE.......Power: " + profile.Power + ".......TIME " + profile.Time.Hours + ":" + profile.Time.Minutes);
+                        console.log("\n||==============================================");
+                        console.log("RUNNING CURRENT SIMULATION DATA SET......Power: " + loadTimeDataSet.Power + ".......TIME " + loadTimeDataSet.Time.Hours + ":" + loadTimeDataSet.Time.Minutes);
 
+                        var date = new Date();
+                        var today = 'today';
+                        var query = {Time:{$gt:{Hours: date.getHours(), Minutes: date.getMinutes()}}};
 
                         //i2c.automaticUpdate(nextProfileToRun);
-                        runNextLoadProfile();
+                        setupNextSimulation(query, today);
 
-                    }, profile.TimeOut);
+                    }, loadTimeDataSet.TimeLeft);
                 }
 
+            }); //getNextSimulationDataSet() ends here
 
-            });
-
-
-        }
+        } //setupNextSimulation() ends here
 
 
-
-
-    }
+    } //timeSheduler ends here
 };
 
 module.exports = LoadProfileManager;
